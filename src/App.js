a3a02ada926945f9547e3c855a2c952d79f29843
3mmyd3v <120048821+3mmyd3v@users.noreply.github.com>
2023-06-07 23:34:17 +0100

    Form
    Finished Form

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React, { Component } from "react";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
